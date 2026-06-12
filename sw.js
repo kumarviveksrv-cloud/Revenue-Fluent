ff34269bebc4fce0b65a5049aa580921cd732d7b
Kumar Vivek <kumarvivek.srv@gmail.com>
2026-06-12 13:14:16 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version: 2026-06-10
 // Enables offline access and PWA install
 
-const CACHE_NAME = 'revenue-fluent-v11';
+const CACHE_NAME = 'revenue-fluent-v12';
 
 const CORE_FILES = [
   '/',
